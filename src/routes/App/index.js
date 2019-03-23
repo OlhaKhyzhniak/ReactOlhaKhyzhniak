4d1fdee0e916b3cd5ff3167ad58d2066fe557958
olha <olya.khizhnyak23@gmail.com>
2019-03-23 13:33:02 +0200

    react start

---
+++ src/routes/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './styles.scss';
+import {Header} from '../Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header></Header>
+      React App
+      </div>
+    );
+  }
+}
+
+export default App;
