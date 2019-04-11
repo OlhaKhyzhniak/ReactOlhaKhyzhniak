4525c82fb6c7984f47c19498f79ba4112595fd25
olha <olya.khizhnyak23@gmail.com>
2019-04-11 21:31:45 +0300

    0

--- src/routes/Header/index.js
+++ src/routes/Header/index.js
@@ -3,6 +3,7 @@ import './styles.scss';
 import {MenuItem} from './MenuItem';
 import logo from '../../utils/images/logo.png';
 import {Col} from 'reactstrap';
+import axios from 'axios';
 var links = [
     'Home',
     'Services',
@@ -11,6 +12,14 @@ var links = [
     'Contact'
 ];
 export class Header extends React.Component{
+    componentDidMount(){
+        axios.get('https://api.github.com/users')
+        .then(response =>{
+            const { data } = response;
+            this.setState({users: data });
+        })
+        .catch((err)=>console.log('somethinh went wrong'));
+    }
     render(){
         return <header>
             <div className='header-wrap'>
